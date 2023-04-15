4726c97d96659c99f89ce04e946101d3d5a6f05b
Elisa <elisa.do@outlook.com>
2023-04-15 15:44:26 +1000

    resume content

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 import AboutMe from './components/AboutMe';
 import Footer from './components/Footer';
-import Portfolio from './components/Portfolio'
-import Contact from './components/Contact'
-
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from "./components/Resume";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MyComponent />
       </header>
-      {/* <AboutMe /> */}
+      <AboutMe />
       {/* <Portfolio /> */}
-      <Contact />
+      {/* <Contact /> */}
+      {/* <Resume /> */}
       <Footer />
     </div>
   );
---
+++ src/components/Resume.js
@@ -0,0 +1,32 @@
+const Resume = () => {
+    return <div>
+        <div class="bodyContainer">
+            <div>Download my <a href="www.google.com">resume</a></div>
+            <div>
+                <h3>Font-end Proficiencies</h3>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>JQuery</li>
+                    <li>Responsive Design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+                <h3>Back-end Proficiencies</h3>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySql, Sequalize</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>REST</li>
+                    <li>GraphQL</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+}
+
+export default Resume
